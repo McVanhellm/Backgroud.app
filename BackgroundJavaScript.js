813ddf16703631e4e22d08143639dc67d9e5d301
McVanhellm <mcavnhellm@gmail.com>
2021-11-11 00:26:55 +0300

    initial commit

---
+++ BackgroundJavaScript.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.blog-slider', {
+ effect: 'fade',
+ loop: true,
+ mousewheel: {
+     invert: false,
+ },
+ autoHeight: true,
+ pagination: {
+     el: '.blog-slider__pagination',
+     clickable: true,
+ }
+});
